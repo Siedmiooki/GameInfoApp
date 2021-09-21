28d4a3c66f6e032e4670ff84539cf740ea232e50
Jan Karpowicz <jan.karpowicz11@gmail.com>
2021-09-21 19:48:39 +0200

    5. styling + global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello GameInfoApp!</h1>
+      <GlobalStyles />
       <Home />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,57 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction"
+import Game from "../components/Game";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
+
     const dispatch = useDispatch();
+
     useEffect(() => {
         dispatch(loadGames())
-    })
+    }, [dispatch])
+
+    const { popularGames, newGames, upcomingGames } = useSelector(state => state.games)
+
     return (
-        <div>
-            <h1>
-                Home
-            </h1>
-        </div>
+        <StyledGameList>
+            <h2>Upcoming Games</h2>
+            <StyledGames>
+                {upcomingGames.map((game) => (
+                    <Game name={game.name} released={game.released} id={game.id} image={game.background_image} key={game.id} />
+                ))}
+            </StyledGames>
+            <h2>Popular Games</h2>
+            <StyledGames>
+                {popularGames.map((game) => (
+                    <Game name={game.name} released={game.released} id={game.id} image={game.background_image} key={game.id} />
+                ))}
+            </StyledGames>
+            <h2>New Games</h2>
+            <StyledGames>
+                {newGames.map((game) => (
+                    <Game name={game.name} released={game.released} id={game.id} image={game.background_image} key={game.id} />
+                ))}
+            </StyledGames>
+        </StyledGameList>
     );
 }
+
+const StyledGameList = styled(motion.div)`
+    padding: 0rem 5rem;
+    h2 {
+        padding: 5rem 0rem;
+    }
+`
+
+const StyledGames = styled(motion.div)`
+    min-height: 80vh;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+    grid-column-gap: 3rem;
+    grid-row-gap: 5rem
+`
 
 export default Home;
